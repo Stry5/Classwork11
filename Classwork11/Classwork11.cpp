9cbcc8fa34615b57d3831f53e51fbdc4d1d4c738
Glenn <glenndavidmah@gmail.com>
2024-09-15 20:12:23 -0700

    first commit

---
+++ Classwork11/Classwork11.cpp
@@ -0,0 +1,21 @@
+// Classwork11.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y;
+	cout << "please inut a value\n";
+	cin >> x;
+	cout << "please input a different value\n";
+	cin >> y;
+	if (x > y) {
+		cout << x;
+	}
+	else {
+		cout << y;
+	}
+
+}
